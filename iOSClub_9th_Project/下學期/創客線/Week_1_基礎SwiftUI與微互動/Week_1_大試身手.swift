fcbdbb3eabd577db41f9283b03789bc7ebefb1f3
Polalalabear <polalalabear@gmail.com>
2026-03-09 22:44:25 +0800

    fix: Second_Semester_Week_1_demo2 content

--- iOSClub_9th_Project/下學期/創客線/Week_1_基礎SwiftUI與微互動/Week_1_大試身手.swift
+++ iOSClub_9th_Project/下學期/創客線/Week_1_基礎SwiftUI與微互動/Week_1_大試身手.swift
@@ -1,8 +1,6 @@
 //
-//  demo_2.swift
+//  demo_2_template.swift
 //  microinteraction
-//
-//  Created by AS Q on 2026/3/8.
 //
 
 import SwiftUI
@@ -13,76 +11,101 @@ struct Second_Semester_Week_1_demo2: View {
     @State private var isReposted: Bool = false
     @State private var repostRotation: Double = 0
     @State private var isShared: Bool = false
-
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(.gray)
-                .frame(width:375, height: 120)
-                .overlay(alignment: .topLeading) {
-                    HStack(alignment: .top, spacing: 12) {
-                        
-                        Image("reindeer")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50)
-                            .clipShape(.circle)
-                        
-                        VStack(alignment: .leading, spacing: 8) {
-                            
-                            Text("Peter")
-                                .font(.headline)
-                                .foregroundStyle(.white)
-                            
-                            Text("大家好我是 Peter，喜歡 tuki.")
-                                .font(.body)
-                                .foregroundStyle(.white)
-                            
-                            HStack(spacing: 20) {
-                                
-                                Image(systemName: isLiked ? "heart.fill" : "heart")
-                                    .foregroundStyle(isLiked ? .red : .white)
-                                    .contentTransition(.symbolEffect(.replace))
-                                    .symbolEffect(.bounce, value: isLiked)
-                                    .onTapGesture {
-                                        withAnimation(.spring) {
-                                            isLiked.toggle()
-                                        }
-                                    }
-                                
-                                Image(systemName: "bubble")
-                                    .symbolEffect(.bounce, value: isCommented)
-                                    .onTapGesture {
-                                        isCommented.toggle()
-                                    }
-                                
-                                Image(systemName: "arrow.2.squarepath")
-                                    .rotationEffect(.degrees(repostRotation))
-                                    .symbolEffect(.bounce, value: isReposted)
-                                    .onTapGesture {
-                                        isReposted.toggle()
-                                        withAnimation(.spring) {
+        TabView{
+            ZStack{
+                Color.gray.opacity(0.5).ignoresSafeArea()
+                ScrollView{
+                    VStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.gray)
+                            .frame(width: 375, height: 120)
+                            .overlay(alignment: .topLeading) {
+                                HStack(alignment: .top, spacing: 12) {
+                                    
+                                    Image("reindeer")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50)
+                                        .clipShape(.circle)
+                                    
+                                    VStack(alignment: .leading, spacing: 8) {
+                                        
+                                        Text("Peter")
+                                            .font(.headline)
+                                            .foregroundStyle(.white)
+                                        
+                                        Text("大家好我是 Peter，喜歡 tuki.")
+                                            .font(.body)
+                                            .foregroundStyle(.white)
+                                        
+                                        HStack(spacing: 20) {
                                             
-                                            repostRotation += 180
+                                            Image(systemName: isLiked ? "heart.fill" : "heart")
+                                                .foregroundStyle(isLiked ? .red : .white)
+                                                .contentTransition(.symbolEffect(.replace))
+                                                .symbolEffect(.bounce, value: isLiked)
+                                                .onTapGesture {
+                                                    withAnimation(.spring) {
+                                                        isLiked.toggle()
+                                                    }
+                                                }
+                                            
+                                            Image(systemName: "bubble")
+                                                .symbolEffect(.bounce, value: isCommented)
+                                                .onTapGesture {
+                                                    isCommented.toggle()
+                                                }
+                                            
+                                            Image(systemName: "arrow.2.squarepath")
+                                                .rotationEffect(.degrees(repostRotation))
+                                                .symbolEffect(.bounce, value: isReposted)
+                                                .onTapGesture {
+                                                    isReposted.toggle()
+                                                    withAnimation(.spring) {
+                                                        
+                                                        repostRotation += 180
+                                                    }
+                                                }
+                                            
+                                            Image(systemName: "paperplane")
+                                                .symbolEffect(.bounce, value: isShared)
+                                                .onTapGesture {
+                                                    isShared.toggle()
+                                                }
                                         }
+                                        .font(.title3)
+                                        .foregroundStyle(.white)
                                     }
-                                
-                                Image(systemName: "paperplane")
-                                    .symbolEffect(.bounce, value: isShared)
-                                    .onTapGesture {
-                                        isShared.toggle()
+                                    if isCommented {
+                                        Text("已留言")
+                                            .font(.caption)
+                                            .foregroundStyle(.black)
                                     }
+                                    
+                                    if isShared {
+                                        Text("已傳送")
+                                            .font(.caption)
+                                            .foregroundStyle(.yellow)
+                                    }
+                                }
+                                .padding()
                             }
-                            .font(.title3)
-                            .foregroundStyle(.white)
-                        }
                     }
-                    .padding()
                 }
-            Text("")
+            }
+            .tabItem {
+                Label("首頁", systemImage: "house.fill")
+            }
+            
+            Text("個人介面")
+                .tabItem {
+                    Label("個人", systemImage: "person")
+                }
         }
     }
 }
+
 
 #Preview {
     Second_Semester_Week_1_demo2()
